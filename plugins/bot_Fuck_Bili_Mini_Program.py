4c7ecba47ffef1b1271541513b44266762c52432
AlinJiong <alinjiong@qq.com>
2021-12-08 21:54:11 +0800

    修改B站的链接图片转换

--- plugins/bot_Fuck_Bili_Mini_Program.py
+++ plugins/bot_Fuck_Bili_Mini_Program.py
@@ -2,6 +2,22 @@ import re
 
 from botoy import GroupMsg, S
 from botoy import async_decorators as deco
+import base64
+import json
+from io import BytesIO
+from pathlib import Path
+
+import httpx
+from PIL import Image, ImageFilter
+from botoy import FriendMsg, GroupMsg, S, jconfig, logger
+from botoy.parser import friend as fp
+from botoy.parser import group as gp
+from httpx_socks import AsyncProxyTransport
+
+import requests
+import re
+from io import BytesIO
+from PIL import Image, ImageFilter
 
 __doc__ = """把B站小程序转成链接"""
 
@@ -124,8 +140,15 @@ RQFFFFAUUUUH/9k=
 @deco.these_msgtypes("XmlMsg")
 async def receive_group_msg(ctx: GroupMsg):
     if info := re.findall(r"(https://b23\.tv/\w*)", ctx.Content):
+        img_url = ctx.Content.split("\"")[24].replace('\\', '')
+        content = requests.request("get", img_url).content
+        with BytesIO() as bf:
+            image = Image.open(BytesIO(content))
+            image.save(bf, format="JPEG")
+            img = base64.b64encode(bf.getvalue()).decode()
+
         await S.bind(ctx).aimage(
-            dont_Send_miniProgram_Base64PIC,
+            img,
             info[0],
             type=S.TYPE_BASE64,
         )
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import httpx
 import base64
 import json
 from PIL import Image, ImageFilter
+import re
 
 # url = "https://v2.alapi.cn/api/zaobao"
 # payload = "token=EFolx1cxAdqqSWqy&format=json"
@@ -31,8 +32,27 @@ from PIL import Image, ImageFilter
 # print(content)
 
 
-url = "http://api.nmb.show/xiaojiejie1.php"
-content = requests.request("get", url).content
+# url = "http://api.nmb.show/xiaojiejie1.php"
+# content = requests.request("get", url).content
+# with BytesIO() as bf:
+#     image = Image.open(BytesIO(content))
+#     image.save(bf, format="JPEG")
+#     img = base64.b64encode(bf.getvalue()).decode()
+#     print(img)
+
+
+str_list = '''
+{"Content":"\u003c?xml version='1.0' encoding='UTF-8' standalone='yes'?\u003e\u003cmsg templateID=\"123\" url=\"https://b23.tv/KdSp3tS?share_medium=android\u0026amp;share_source=qq\u0026amp;bbid=XXB8AC5BE894D54048AE47A6867AE6170B60B\u0026amp;ts=1638968909599\" serviceID=\"1\" action=\"web\" actionData=\"\" a_actionData=\"\" i_actionData=\"\" brief=\"[QQ小程序]哔哩哔哩\" flag=\"0\"\u003e\u003citem layout=\"2\"\u003e\u003cpicture cover=\"http://pubminishare-30161.picsz.qpic.cn/8651bcb5-6ce2-43dc-aae9-9b79c6bf21cd\"/\u003e\u003ctitle\u003e哔哩哔哩\u003c/title\u003e\u003csummary\u003e那天我戴着头套排队做核酸……社死了但没完全社死\u003c/summary\u003e\u003c/item\u003e\u003csource url=\"https://b23.tv/KdSp3tS?share_medium=android\u0026amp;share_source=qq\u0026amp;bbid=XXB8AC5BE894D54048AE47A6867AE6170B60B\u0026amp;ts=1638968909599\" icon=\"https://open.gtimg.cn/open/app_icon/00/95/17/76/100951776_100_m.png?t=1638951012\" name=\"哔哩哔哩\" appid=\"0\" action=\"web\" actionData=\"\" a_actionData=\"tencent0://\" i_actionData=\"\"/\u003e\u003c/msg\u003e"}
+'''
+
+info = re.findall(r"(https://b23\.tv/\w*)", str_list)
+
+for i in range(len(str_list.split("\""))):
+    print(i, str_list.split("\"")[i])
+# print(str_list.split("\\")[31])
+
+img_url = str_list.split("\"")[24]
+content = requests.request("get", img_url).content
 with BytesIO() as bf:
     image = Image.open(BytesIO(content))
     image.save(bf, format="JPEG")
