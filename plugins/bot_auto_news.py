0ff7e4cac4d1ac2ecaccbae2d0f97922b32ceb89
AlinJiong <alinjiong@qq.com>
2021-12-17 18:05:04 +0800

    修改news

--- plugins/bot_auto_news.py
+++ plugins/bot_auto_news.py
@@ -26,39 +26,48 @@ from botoy import Action
 __doc__ = "自动发送早报"
 
 
-class SearchNews:
+def get_news():
+    url = "https://v2.alapi.cn/api/zaobao"
+    payload = "token=EFolx1cxAdqqSWqy&format=json"
+    headers = {'Content-Type': "application/x-www-form-urlencoded"}
+    response = requests.request("POST", url, data=payload, headers=headers)
+    text_to_dic = json.loads(response.text)
+    img_url = text_to_dic['data']['image']
+    content = httpx.get(img_url).content
 
-    def get_news():
-        url = "https://v2.alapi.cn/api/zaobao"
-        payload = "token=EFolx1cxAdqqSWqy&format=json"
-        headers = {'Content-Type': "application/x-www-form-urlencoded"}
-        response = requests.request("POST", url, data=payload, headers=headers)
-        text_to_dic = json.loads(response.text)
-        img_url = text_to_dic['data']['image']
-
-        content = httpx.get(img_url).content
-        with BytesIO() as bf:
-            image = Image.open(BytesIO(content))
-            if image.format == 'WEBP':
-                image.save(bf, format="JPEG")
-                img = base64.b64encode(bf.getvalue()).decode()
-                action = Action(qq=461505108)
-                try:
-                    action.sendGroupPic(
-                        544830164, content="#今日早报#", picBase64Buf=img)
-                    action.sendGroupPic(
-                        257069779, content="#今日早报#", picBase64Buf=img)
-                    # action.sendGroupPic(
-                    #     953219612, content="#今日早报#", picBase64Buf=img)
-                    action.sendFriendPic(
-                        2311366525, content="#今日早报#", picBase64Buf=img)
-                except:
-                    del action
+    with BytesIO() as bf:
+        image = Image.open(BytesIO(content))
+        if image.format == 'WEBP':
+            image.save(bf, format="JPEG")
+            img = base64.b64encode(bf.getvalue()).decode()
+            return img
 
 
-job1 = scheduler.add_job(
-    SearchNews.get_news, 'cron', hour=9, minute=0)
+def send_news():
+    action = Action(qq=461505108)
+    img = get_news()
+    try:
+        # action.sendGroupPic(544830164, content="#今日早报#", picBase64Buf=img)
+        # action.sendGroupPic(257069779, content="#今日早报#", picBase64Buf=img)
+        # action.sendGroupPic(953219612, content="#今日早报#", picBase64Buf=img)
+        action.sendFriendPic(2311366525, content="#今日早报#", picBase64Buf=img)
+        del action
+        logger.info("发送早报成功！")
+    except:
+        logger.info("发送早报失败！")
 
-# job1 = scheduler.add_job(
-#     SearchNews.get_news, 'interval', minutes=1)
-# job2 = scheduler.add_job(lambda: print("我一分钟出现一次"), 'interval', minutes=1)
+
+job1 = scheduler.add_job(send_news(), 'cron', hour=16, minute=35)
+
+# job1 = scheduler.add_job(get_news, 'interval', minutes=1)
+
+# if not flag:
+#     action = Action(qq=461505108)
+#     img = get_news()
+#     # action.sendGroupPic(544830164, content="#今日早报#", picBase64Buf=img)
+#     # action.sendGroupPic(257069779, content="#今日早报#", picBase64Buf=img)
+#     # action.sendGroupPic(953219612, content="#今日早报#", picBase64Buf=img)
+#     action.sendFriendPic(2311366525, content="#今日早报#", picBase64Buf=img)
+#     del action
+
+#job2 = scheduler.add_job(lambda: print("我一分钟出现一次"), 'interval', minutes=1)
