f76bd5affc6d4e0fcdd28f99441d70cfa76938e3
AlinJiong <alinjiong@qq.com>
2022-02-18 11:35:52 +0800

    早报api替换

--- plugins/bot_auto_news.py
+++ plugins/bot_auto_news.py
@@ -32,39 +32,13 @@ __doc__ = "自动发送早报"
 
 
 async def get_news():
-    url = "https://v2.alapi.cn/api/zaobao"
-    payload = "token=1jfSWghgtebOjpQi&format=json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-               'Content-Type': "application/x-www-form-urlencoded"}
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(url, data=payload, headers=headers, timeout=10)
-        except:
-            logger.info("早报api获取失败！")
-            return None
-        # if response.status_code != 200:
-        #     logger.info("早报获取失败！")
-        #     return None
-        text_to_dic = json.loads(response.text)
-        img_url = text_to_dic['data']['image']
-
-        try:
-            new_headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-            }
-
-            content = await client.get(img_url, headers=new_headers, timeout=10)
-            content = content.content
-            with BytesIO() as bf:
-                image = Image.open(BytesIO(content))
-                if image.format == 'WEBP':
-                    image.save(bf, format="JPEG")
-                    img = base64.b64encode(bf.getvalue()).decode()
-                    logger.info("获取早报成功！")
-                    return img
-        except:
-            logger.info("早报api转图片失败！")
-            return None
+    url = 'http://dwz.2xb.cn/zaob'
+    content = requests.get(url)
+    text_to_dic = json.loads(content.text)
+    img_url = text_to_dic['imageUrl']
+    if text_to_dic['code'] != 200:
+        return None
+    return img_url
 
 
 async def send_news():
@@ -81,14 +55,14 @@ async def send_news():
     try:
         for group in groups:
             try:
-                action.sendGroupPic(group, content="#今日早报#", picBase64Buf=img)
+                action.sendGroupPic(group, content="#今日早报#", picUrl=img)
                 time.sleep(3)
                 logger.info("发送"+str(group)+"早报成功！")
             except:
                 pass
 
         action.sendFriendPic(jconfig.superAdmin,
-                             content="#今日早报#", picBase64Buf=img)
+                             content="#今日早报#", picUrl=img)
 
         logger.info("发送早报成功！")
     except:
@@ -116,11 +90,11 @@ async def send_news_to_one():
             pass
 
         for user in users:
-            action.sendFriendPic(user, content="#今日早报#", picBase64Buf=img)
+            action.sendFriendPic(user, content="#今日早报#", picUrl=img)
             time.sleep(3)
             logger.info(f'向好友：{user} 发送早报！')
         # Action(qq=jconfig.bot).sendFriendPic(
-        #     2382194151, content="#今日早报#", picBase64Buf=img)
+        #     2382194151, content="#今日早报#", picUrl=img)
         logger.info("发送7点早报成功！")
     except:
         pass
@@ -151,10 +125,10 @@ job2 = scheduler.add_job(fun2, 'cron', hour=7, minute=0)
 # if not flag:
 #     action = Action(qq=461505108)
 #     img = get_news()
-#     # action.sendGroupPic(544830164, content="#今日早报#", picBase64Buf=img)
-#     # action.sendGroupPic(257069779, content="#今日早报#", picBase64Buf=img)
-#     # action.sendGroupPic(953219612, content="#今日早报#", picBase64Buf=img)
-#     action.sendFriendPic(2311366525, content="#今日早报#", picBase64Buf=img)
+#     # action.sendGroupPic(544830164, content="#今日早报#", picUrl=img)
+#     # action.sendGroupPic(257069779, content="#今日早报#", picUrl=img)
+#     # action.sendGroupPic(953219612, content="#今日早报#", picUrl=img)
+#     action.sendFriendPic(2311366525, content="#今日早报#", picUrl=img)
 #     del action
 
 # job2 = scheduler.add_job(lambda: print("我一分钟出现一次"), 'interval', minutes=1)
--- plugins/bot_search_news.py
+++ plugins/bot_search_news.py
@@ -7,6 +7,7 @@ import psutil
 from botoy import S
 from botoy import async_decorators as deco
 from botoy import logger
+from sympy import im
 
 __doc__ = "发送 早报 查看新闻"
 
@@ -26,36 +27,14 @@ import gc
 
 
 async def get_news():
-    url = "https://v2.alapi.cn/api/zaobao"
-    payload = "token=1jfSWghgtebOjpQi&format=json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-               'Content-Type': "application/x-www-form-urlencoded"}
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(url, data=payload, headers=headers, timeout=10)
-            text_to_dic = json.loads(response.text)
-            img_url = text_to_dic['data']['image']
-        except:
-            logger.info("早报api获取失败！")
-            return None
+    url = 'http://dwz.2xb.cn/zaob'
+    content = requests.get(url)
+    text_to_dic = json.loads(content.text)
+    img_url = text_to_dic['imageUrl']
+    if text_to_dic['code'] != 200:
+        return None
+    return img_url
 
-        try:
-            new_headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-            }
-
-            content = await client.get(img_url, headers=new_headers, timeout=10)
-            content = content.content
-            with BytesIO() as bf:
-                image = Image.open(BytesIO(content))
-                if image.format == 'WEBP':
-                    image.save(bf, format="JPEG")
-                    img = base64.b64encode(bf.getvalue()).decode()
-                    logger.info("获取早报成功！")
-                    return img
-        except:
-            logger.info("早报api转图片失败！")
-            return None
 
 # @deco.ignore_botself
 # @deco.equal_content("早报")
@@ -84,7 +63,7 @@ async def receive_group_msg(ctx: GroupMsg):
             ctx.FromGroupId, content="网络响应超时，请稍后重试！")
     else:
         Action(ctx.CurrentQQ).sendGroupPic(ctx.FromGroupId,
-                                           content="#今日早报#", picBase64Buf=img)
+                                           content="#今日早报#", picUrl=img)
     del img
     gc.collect()
 
@@ -98,6 +77,6 @@ async def receive_friend_msg(ctx: FriendMsg):
             ctx.FromUin, content="网络响应超时，请稍后重试！")
     else:
         Action(ctx.CurrentQQ).sendFriendPic(
-            ctx.FromUin, content="#今日早报#", picBase64Buf=img)
+            ctx.FromUin, content="#今日早报#", picUrl=img)
     del img
     gc.collect()
