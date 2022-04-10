cda21a42135bc049a99a33017a4bdafc676891a9
AlinJiong <alinjiong@qq.com>
2022-04-10 09:12:08 +0800

    更换接口

--- plugins/bot_autoHostList.py
+++ plugins/bot_autoHostList.py
@@ -21,50 +21,62 @@ from botoy.contrib import sync_run
 __doc__ = "自动推送 微博热搜 早9点，晚7点"
 
 
-def url_encode(url: str):
-    "链接encode"
-    return urllib.parse.quote(url, encoding="utf-8")
+# def url_encode(url: str):
+#     "链接encode"
+#     return urllib.parse.quote(url, encoding="utf-8")
 
+
+# async def long_to_short(url: str):
+#     "长链接转短链接"
+#     api_url = "http://api.suowo.cn/api.htm?url="
+
+#     encode_url = url_encode(url)
+#     key = "61d6c080aee1e862935dab34@9ee40add55f5e3eb3580deb02fa3658b"
+
+#     # 第二天 = today + 2
+#     date_after = datetime.date.today() + datetime.timedelta(days=1)
+#     # 格式转换
+
+#     expireDate = date_after.strftime("%Y-%m-%d")
+
+#     final_url = api_url+encode_url+'&key=' + \
+#         key+'&expireDate='+expireDate+'&domain=5'
+
+#     # headers = {
+#     #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+#     # }
+
+#     t = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
+#          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+#          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43']
+
+#     headers = {}
+#     headers['User-Agent'] = t[random.randint(0, len(t)-1)]
+
+#     async with httpx.AsyncClient() as client:
+#         try:
+#             response = await client.get(final_url, headers=headers, timeout=10)
+#             return str(response.text)
+#         except:
+#             logger.info("长链接转短链接异常！")
+#             return url
+
+#     #     if len(str(response.text)) == 0 or len(str(response.text)) > 40:
+#     #         return url
+
+#     # return str(response.text)
 
 async def long_to_short(url: str):
     "长链接转短链接"
-    api_url = "http://api.suowo.cn/api.htm?url="
 
-    encode_url = url_encode(url)
-    key = "61d6c080aee1e862935dab34@9ee40add55f5e3eb3580deb02fa3658b"
+    request_url = "https://alinjiong.ml:8080/api/l2s?long="+url
 
-    # 第二天 = today + 2
-    date_after = datetime.date.today() + datetime.timedelta(days=1)
-    # 格式转换
-
-    expireDate = date_after.strftime("%Y-%m-%d")
-
-    final_url = api_url+encode_url+'&key=' + \
-        key+'&expireDate='+expireDate+'&domain=5'
-
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-    # }
-
-    t = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
-         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43']
-
-    headers = {}
-    headers['User-Agent'] = t[random.randint(0, len(t)-1)]
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(final_url, headers=headers, timeout=10)
-            return str(response.text)
-        except:
-            logger.info("长链接转短链接异常！")
-            return url
-
-    #     if len(str(response.text)) == 0 or len(str(response.text)) > 40:
-    #         return url
-
-    # return str(response.text)
+    try:
+        response = requests.get(request_url, timeout=10)
+        _dic = json.loads(response.text)
+        return str(_dic['short'])
+    except:
+        return url
 
 
 async def get_HotList(choice: str = 'weibo'):
@@ -88,7 +100,7 @@ async def get_HotList(choice: str = 'weibo'):
                 logger.info(link)
                 content += str(i)+'.' + data[i]['title'] + \
                     '\n' + link + '\n'
-                time.sleep(random.randint(5, 8))
+                time.sleep(1)
 
             action = Action(qq=jconfig.bot)
             action.sendGroupText(257069779, content)
