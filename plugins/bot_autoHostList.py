5389f471bc8a5be6c9e1d1584d8a387f4f313f4e
AlinJiong <alinjiong@qq.com>
2022-04-16 16:07:55 +0800

    恢复自定义长链接转短链接接口

--- plugins/bot_autoHostList.py
+++ plugins/bot_autoHostList.py
@@ -21,99 +21,41 @@ from botoy.contrib import sync_run
 __doc__ = "自动推送 微博热搜 早9点，晚7点"
 
 
-def url_encode(url: str):
-    "链接encode"
-    return urllib.parse.quote(url, encoding="utf-8")
-
-
-async def long_to_short(url: str):
-    "长链接转短链接"
-    api_url = "http://api.suowo.cn/api.htm?url="
-
-    encode_url = url_encode(url)
-    key = "61d6c080aee1e862935dab34@9ee40add55f5e3eb3580deb02fa3658b"
-
-    # 第二天 = today + 2
-    date_after = datetime.date.today() + datetime.timedelta(days=1)
-    # 格式转换
-
-    expireDate = date_after.strftime("%Y-%m-%d")
-
-    final_url = api_url+encode_url+'&key=' + \
-        key+'&expireDate='+expireDate+'&domain=5'
-
-    t = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
-         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
-         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43']
-
-    headers = {}
-    headers['User-Agent'] = t[random.randint(0, len(t)-1)]
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(final_url, headers=headers, timeout=10)
-            return str(response.text)
-        except:
-            logger.info("长链接转短链接异常！")
-            return url
-
-
-async def get_HotList(choice: str = 'weibo'):
-    "获取微博热搜"
-    url = "https://v2.alapi.cn/api/tophub/get"
-    payload = "token=1jfSWghgtebOjpQi&type=" + choice
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
-               'Content-Type': "application/x-www-form-urlencoded"}
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(url, data=payload, headers=headers, timeout=10)
-            text_to_dic = json.loads(response.text)
-            data = text_to_dic['data']['list']
-            # logger.info(data[0]['link'])
-
-            content = "#实时微博热搜#\n"
-
-            for i in range(0, 10):
-                link = await long_to_short(data[i]['link'])
-                logger.info(link)
-                content += str(i)+'.' + data[i]['title'] + \
-                    '\n' + link + '\n'
-                time.sleep(random.randint(5, 8))
-
-            action = Action(qq=jconfig.bot)
-            action.sendGroupText(257069779, content)
-            time.sleep(5)
-            action.sendFriendText(jconfig.superAdmin, content)
-
-            del content, action
-            gc.collect()
-        except:
-            logger.info('自动获取微博热搜失败！')
-            return None
-
-
-def func1():
-    sync_run(get_HotList())
-
-
-# v2.0
-# job1 = scheduler.add_job(func1, 'cron', hour=9, minute=5)
-
-# job2 = scheduler.add_job(func1, 'cron', hour=19, minute=0)
+# def url_encode(url: str):
+#     "链接encode"
+#     return urllib.parse.quote(url, encoding="utf-8")
 
 
 # async def long_to_short(url: str):
 #     "长链接转短链接"
+#     api_url = "http://api.suowo.cn/api.htm?url="
 
-#     request_url = "https://alinjiong.ml:8080/api/l2s?long="+url
+#     encode_url = url_encode(url)
+#     key = "61d6c080aee1e862935dab34@9ee40add55f5e3eb3580deb02fa3658b"
 
-#     try:
-#         response = requests.get(request_url, timeout=10)
-#         _dic = json.loads(response.text)
-#         return str(_dic['short'])
-#     except:
-#         return url
+#     # 第二天 = today + 2
+#     date_after = datetime.date.today() + datetime.timedelta(days=1)
+#     # 格式转换
+
+#     expireDate = date_after.strftime("%Y-%m-%d")
+
+#     final_url = api_url+encode_url+'&key=' + \
+#         key+'&expireDate='+expireDate+'&domain=5'
+
+#     t = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
+#          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+#          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43']
+
+#     headers = {}
+#     headers['User-Agent'] = t[random.randint(0, len(t)-1)]
+
+#     async with httpx.AsyncClient() as client:
+#         try:
+#             response = await client.get(final_url, headers=headers, timeout=10)
+#             return str(response.text)
+#         except:
+#             logger.info("长链接转短链接异常！")
+#             return url
 
 
 # async def get_HotList(choice: str = 'weibo'):
@@ -137,7 +79,7 @@ def func1():
 #                 logger.info(link)
 #                 content += str(i)+'.' + data[i]['title'] + \
 #                     '\n' + link + '\n'
-#                 time.sleep(1)
+#                 time.sleep(random.randint(5, 8))
 
 #             action = Action(qq=jconfig.bot)
 #             action.sendGroupText(257069779, content)
@@ -155,6 +97,64 @@ def func1():
 #     sync_run(get_HotList())
 
 
+
 # job1 = scheduler.add_job(func1, 'cron', hour=9, minute=5)
 
 # job2 = scheduler.add_job(func1, 'cron', hour=19, minute=0)
+
+# v2.0
+async def long_to_short(url: str):
+    "长链接转短链接"
+
+    request_url = "https://alinjiong.ml:8080/api/l2s?long="+url
+
+    try:
+        response = requests.get(request_url, timeout=10)
+        _dic = json.loads(response.text)
+        return str(_dic['short'])
+    except:
+        return url
+
+
+async def get_HotList(choice: str = 'weibo'):
+    "获取微博热搜"
+    url = "https://v2.alapi.cn/api/tophub/get"
+    payload = "token=1jfSWghgtebOjpQi&type=" + choice
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+               'Content-Type': "application/x-www-form-urlencoded"}
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url, data=payload, headers=headers, timeout=10)
+            text_to_dic = json.loads(response.text)
+            data = text_to_dic['data']['list']
+            # logger.info(data[0]['link'])
+
+            content = "#实时微博热搜#\n"
+
+            for i in range(0, 10):
+                link = await long_to_short(data[i]['link'])
+                logger.info(link)
+                content += str(i)+'.' + data[i]['title'] + \
+                    '\n' + link + '\n'
+                time.sleep(1)
+
+            action = Action(qq=jconfig.bot)
+            action.sendGroupText(257069779, content)
+            time.sleep(5)
+            action.sendFriendText(jconfig.superAdmin, content)
+
+            del content, action
+            gc.collect()
+        except:
+            logger.info('自动获取微博热搜失败！')
+            return None
+
+
+def func1():
+    sync_run(get_HotList())
+
+
+job1 = scheduler.add_job(func1, 'cron', hour=9, minute=5)
+
+job2 = scheduler.add_job(func1, 'cron', hour=19, minute=0)
