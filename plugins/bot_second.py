1c72afe8ba3485580cc1cbca2797f2b2e46e38ab
AlinJiong <alinjiong@qq.com>
2022-05-28 13:53:45 +0800

    二次元接口

---
+++ plugins/bot_second.py
@@ -0,0 +1,32 @@
+from botoy import logger
+from botoy import S
+from botoy import decorators
+import requests
+import json
+from botoy import FriendMsg, GroupMsg, S, jconfig, logger
+from botoy.decorators import equal_content, ignore_botself
+from botoy.session import SessionHandler, ctx, session
+import gc
+from botoy import async_decorators as deco
+import httpx
+from botoy import Action, AsyncAction
+
+__doc__ = "二次元"
+
+setuPattern = "二[次刺][元猿]"
+
+
+@deco.ignore_botself
+@deco.on_regexp(setuPattern)
+async def receive_group_msg(ctx: GroupMsg):
+    action = Action(qq=jconfig.bot)
+    action.sendGroupPic(
+        ctx.FromGroupId, picUrl='https://www.dmoe.cc/random.php')
+
+
+@deco.ignore_botself
+@deco.on_regexp(setuPattern)
+async def receive_friend_msg(ctx: FriendMsg):
+    action = Action(qq=jconfig.bot)
+    action.sendFriendPic(
+        ctx.FromUin, picUrl='https://www.dmoe.cc/random.php')
