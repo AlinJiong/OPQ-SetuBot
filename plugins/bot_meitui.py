e063aef551ce71c3d44c45743250dc5eafcf6ac8
AlinJiong <alinjiong@qq.com>
2022-06-08 20:47:42 +0800

    修改美腿图片api接口

--- plugins/bot_meitui.py
+++ plugins/bot_meitui.py
@@ -1,3 +1,4 @@
+from urllib import response
 from botoy import logger
 from botoy import S
 from botoy import decorators
@@ -14,18 +15,33 @@ from botoy import Action, AsyncAction
 __doc__ = "看看腿"
 
 
+async def getPicUrl():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
+    response = requests.get(url='http://yuqingapi.xyz/api/tu.php',
+                            headers=headers, timeout=10)
+    if response.status_code != 200:
+        logger.info('获取美腿图片异常')
+        return None
+    return response.text
+
+
 @deco.ignore_botself
 @deco.from_these_groups(953219612, 815234602)
 @deco.equal_content("看看腿")
 async def receive_group_msg(ctx: GroupMsg):
     action = Action(qq=jconfig.bot)
-    action.sendGroupPic(
-        ctx.FromGroupId, picUrl='https://api.5s9s.com/product/api?&apikey=8715f1e155618c19e563cca1fae62f4e')
+    picUrl = await getPicUrl()
+    if picUrl:
+        action.sendGroupPic(
+            ctx.FromGroupId, picUrl=picUrl)
 
 
 @deco.ignore_botself
 @deco.equal_content("看看腿")
 async def receive_friend_msg(ctx: FriendMsg):
     action = Action(qq=jconfig.bot)
-    action.sendFriendPic(
-        ctx.FromUin, picUrl='https://api.5s9s.com/product/api?&apikey=8715f1e155618c19e563cca1fae62f4e')
+    picUrl = await getPicUrl()
+    if picUrl:
+        action.sendFriendPic(
+            ctx.FromUin, picUrl=picUrl)
