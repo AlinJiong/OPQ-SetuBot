ab7883f798e60c7a6dfd44af883dce786f346bce
AlinJiong <alinjiong@qq.com>
2022-07-19 23:00:00 +0800

    控制图片在5张

--- plugins/bot_mm.py
+++ plugins/bot_mm.py
@@ -34,7 +34,7 @@ async def get_img_url():
         img_list = re.findall(pattern, res.text)
         img_list = list(set(img_list))
         logger.info(img_list)
-        if len(img_list) >= 7:
+        if len(img_list) >= 6:
             return img_list[:5]
         else:
             return img_list
