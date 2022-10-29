0dafa56e6aedf1e1358df191e227fa826209d6f5
AlinJiong <alinjiong@qq.com>
2022-10-29 23:01:17 +0800

    自定义两个sh文件

---
+++ plugins/clean.sh
@@ -0,0 +1,7 @@
+cd /home/T1/bot
+rm -rf nohup.out
+rm -rf Logs
+
+cd /home/T1/setu
+rm -rf nohup.out
+rm -rf logs
---
+++ plugins/start.sh
@@ -0,0 +1,5 @@
+cd /home/T1/bot
+nohup ./OPQBot &
+
+cd /home/T1/setu
+nohup python bot.py &
